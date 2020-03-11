0f6ddc5a5bf93b17079a0ca2b8d6cf104fc99fe8
Matuk <mateusmatuk@fai-mg.br>
2020-03-11 20:55:08 -0300

    exercicio nao finalizado

--- src/br/fai/collections/Animal.java
+++ src/br/fai/collections/Animal.java
@@ -5,4 +5,37 @@ public abstract class Animal {
 	public void som() {
 		System.out.println("Este é o som padrão de animal: zzzzz");
 	}
+
+	public void dizerMeuNome() {
+		System.out.println("O nome do animal é: XXX");
+	}
+
+	private void dizerMinhaIdade() {
+		System.out.println("Minha idade é: XXX");
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	private String cor = "preto";
+	private int idade = 5;
+	private String nome = "Não definido";
+
 }
